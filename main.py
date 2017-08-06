3d90add7f4e24db639617c7cb1c298ea6d776973
Charles Click <CharlesClick@vertinext.com>
2017-08-05 21:24:26 -0400

    Scaffolding
    - Add Basic Structure

--- main.py
+++ main.py
@@ -1,16 +1,54 @@
 import discord
+from discord.ext import commands
 
-client = discord.Client()
+# For future use
+# Do not modify unless you understand
+# startup_extensions = []
 
-token = input('gib token')
+# Make sure we have an overused and annoying prefix
+# Add useless description
+bot = commands.Bot(command_prefix="!", description="Hi! I'm a bot.")
 
-rude_people = [122122926760656896, 103675685343612928] # devon and borked
+# Obligatory Load Command that gives no feedback
+@bot.command()
+async def load(ctx, what : str):
+    """Load a Module"""
+    try:
+        bot.load_extension(what)
+    except ImportError as e:
+        pass
 
-@client.event
-async def on_message(message):
-    await message.channel.send(f'fuck you {message.author.mention}')
+# Obligatory Unload Command that gives no feedback
+@bot.command()
+async def unload(ctx, what : str):
+    """Unload Modules"""
+    bot.unload_extension(what)
 
-    if message.author.id in rude_people:
-        await message.channel.send('double fuck you')
+# Everyone needs a command to disconnect their bot, let's make ours frustrating
+@bot.command()
+async def hidethepainharoldandkillyourself(ctx):
+    """Kill the selfbot and potentially end harold's suffering"""
+    await bot.logout()
 
-client.run(token)
+# Obligatory status editing command
+@bot.command()
+async def status(ctx, what : str):
+    """Set status"""
+    await ctx.message.delete()
+    if bot.is_ready():
+        await bot.change_presence(game=discord.Game(name=what))
+
+# Obligatory on ready, make sure this gives a super inane message
+@bot.event
+async def on_ready():
+    print("Yo boi! We up in dis, fam!")
+
+# Unnecessary namespace comparison for future use
+# Definitely do not edit if unsure of purpose
+if __name__ == "__main__":
+    pass
+
+    # Obligatory token inclusion
+    # Is this a real token?
+    # I don't know, who is going to actually try it out?
+    bot.run("W10xv01289Gqw01tVas078510vASG07vb123tVa0sg701gv13t07va")
